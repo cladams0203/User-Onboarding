21031a772018356f4f76d162b6c2a88edcba1619
Chris Adams <cladams0203@gmail.com>
2019-09-10 13:34:33 -0400

    added Yup and checked the validation

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import { withFormik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 
 
 function Input({values, errors, touched}) {
 
     return (
         <Form>
+        <div>
+            {touched.name && errors.name && <p>{errors.name}</p>}
             <Field type='text' name='name' placeholder='User Name' />
+        </div>
+        <div>
+            {touched.email && errors.email && <p>{errors.email}</p>}
             <Field type='email' name='email' placeholder='User Email' />
+        </div>
+        <div>
+            {touched.password && errors.password && <p>{errors.password}</p>}
             <Field type='password' name='password' placeholder='Password' />
+        </div>
             <label>
                 <Field type='checkbox' name='terms' checked={values.terms} />
                 I Accept The Terms of Service.
             </label>
-            <button>Submit</button>
+        <button type='submit'>Submit</button>
         </Form>
     )
 }
@@ -26,7 +36,22 @@ const UserForm = withFormik({
             password: password || '',
             terms: terms || false
         }
+    },
+    validationSchema: Yup.object().shape({
+        name: Yup.string()
+            .required('User Name is Required'),
+        email: Yup.string()
+            .email('Email not Valid')
+            .required('Email is Required'),
+        password: Yup.string()
+            .min(8, 'Password Must Be 8 Characters or Longer')
+            .required('Password is Required')
+    }),
+
+    handleSubmit(values) {
+        console.log(values)
     }
+    
 })(Input);
 
 export default UserForm;
